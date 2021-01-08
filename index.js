08bd9f1e3c07d815d6b9056b5c16166b61306d3e
Chaitanya31612 <guptachaitanya31@gmail.com>
2021-01-08 11:13:49 +0530

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+// function factorial(number){
+//     let fact= 1;
+//     for(let i=1;i<=number;i++)
+//     {
+//         fact= fact*i;
+//     }
+//     return fact;
+// }
+// console.log(factorial(3));
+// function arm(){
+//     let num = 371;
+//     let t=num;
+//     let s=0;
+//     while(num!=0){
+//         var r= num%10;
+//         s= s + r*r*r;
+//         num= Math.floor(num/10);
+//     }
+//     return s;
+// }
+// console.log(arm());
+
+console.log(3%10);
+
+
+
+
+
